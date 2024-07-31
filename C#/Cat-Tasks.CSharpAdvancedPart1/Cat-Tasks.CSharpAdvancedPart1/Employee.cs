e72bd903ff3f4597b14d82cdee6a823daf048a3c
m-anass <manas8269@gmail.com>
2024-07-31 20:34:44 +0300

    Practise on delegate, event and generics

---
+++ C#/Cat-Tasks.CSharpAdvancedPart1/Cat-Tasks.CSharpAdvancedPart1/Employee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Cat_Tasks.CSharpAdvancedPart1
+{
+    public class Employee
+    {
+        // Normal delegete
+        public delegate bool IsElligeble(Employee e);
+        public delegate void SalaryChangedHandler(Employee e, decimal oldSalary);
+        // event
+        public event SalaryChangedHandler OnSalaryChanged;
+        // Addition task Combine these concepts into a single task, such as creating a generic event handler using a lambda expression.
+        public delegate void AnythingChangedHadler<in T, in T2>(T newValue, T2 oldValue); // Generic delegate 
+
+        public event Action<Employee, string> OnEmployeeNameChanged; // event Using generic delegate that language support
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int TotalSales { get; set; }
+        public decimal Salary { get; set; }
+
+        public void ChangeEmployeeNameTo(string newName)
+        {
+            var oldName = this.Name;
+            this.Name = newName;
+            if (OnEmployeeNameChanged != null)
+                OnEmployeeNameChanged(this, oldName);
+        }
+        public  void ChangeSalaryTo(decimal newSalary)
+        {
+            decimal oldSalary = Salary;
+            Salary = newSalary;
+            if (OnSalaryChanged != null)
+                OnSalaryChanged(this, oldSalary);
+        }
+        public override string ToString()
+        {
+            return $"\nID \t Name \t Age \t Total Sales \t Salary" +
+                $"\n{ID} \t {Name} \t {Age} \t {TotalSales} \t {Salary}";
+        }
+        public static void EmployeeSales(MyList<Employee> emps, string title, Predicate<Employee> isElligeble) // Using generic delegate instead of normal delegate we intialized above
+        {
+            Console.WriteLine(title);
+
+            foreach (var e in emps.Items)
+            {
+                if (isElligeble(e))
+                    Console.WriteLine($"{e}");
+            }
+        }
+    }
+}
---
+++ C#/Cat-Tasks.CSharpAdvancedPart1/Cat-Tasks.CSharpAdvancedPart1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Tasks.CSharpAdvancedPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var Emps = new MyList<Employee>();
+            Emps.Add(new Employee() { Age = 25, TotalSales = 140000, Name = "anas", ID = 10, Salary = 3000 });
+            Emps.Add(new Employee() { Age = 26, TotalSales = 205000, Name = "Ahmed", ID = 11, Salary = 5999 });
+            Emps.Add(new Employee() { Age = 27, TotalSales = 40400, Name = "Ali", ID = 12, Salary = 4988 });
+            Emps.Print();
+
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            Employee.EmployeeSales(Emps, "greater than 50000", e => e.TotalSales > 50000); // Anonymos delegate using lambda experession
+           
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+           
+            Employee.EmployeeSales(Emps, "Between 50000 and 200000", e => e.TotalSales > 50000 && e.TotalSales < 200000);
+            
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            
+            Emps.Items[1].OnSalaryChanged += Emp_OnSalaryChanged;
+            Emps.Items[1].ChangeSalaryTo(8000);
+           
+            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+           
+            Emps.Items[2].OnEmployeeNameChanged += Emp_OnEmployeeNameChanged;
+            Emps.Items[2].ChangeEmployeeNameTo("Ibrahim");
+
+
+            Console.ReadKey();
+        }
+
+        // Generic Event Handler
+        private static void Emp_OnEmployeeNameChanged(Employee newValue, string oldValue)
+        {
+            if(newValue.Name != oldValue)
+                Console.WriteLine($"{oldValue}\n change the name to {newValue}");
+            else
+                Console.WriteLine("nothing changed");
+        }
+
+        // Event handler
+        private static void Emp_OnSalaryChanged(Employee e, decimal oldSalary)
+        {
+            if (e.Salary > oldSalary)
+            {
+                Console.WriteLine($"Congratus You got {e.Salary - oldSalary} bonus , Your Salary now is {e.Salary}");
+            }
+            else if(e.Salary< oldSalary)
+            {
+                Console.WriteLine($"YOu got a {oldSalary - e.Salary} deduction, Your Salary now is {e.Salary}");
+            }
+        }
+    }
+}
