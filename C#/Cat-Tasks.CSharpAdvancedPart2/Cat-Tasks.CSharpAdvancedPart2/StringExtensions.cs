328c139685166195035ec41b4f9383a743209775
m-anass <manas8269@gmail.com>
2024-08-02 18:36:34 +0300

    Practise on Extension Methods

---
+++ C#/Cat-Tasks.CSharpAdvancedPart2/Cat-Tasks.CSharpAdvancedPart2/StringExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Tasks.CSharpAdvancedPart2
+{
+    internal static class StringExtensions
+    {
+        static bool IsValidISBN(this string ISBN)
+        {
+            if (IsValidISBN_10(ISBN) || IsValidISBN_13(ISBN))
+                return true;
+            return false;
+        }
+
+        private static bool IsValidISBN_13(string iSBN)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValidISBN_10(string iSBN)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
