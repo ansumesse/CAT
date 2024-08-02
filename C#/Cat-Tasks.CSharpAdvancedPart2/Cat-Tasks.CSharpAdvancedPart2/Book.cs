a644f4b2213044fe014591e2b62a2e5273d3ae41
m-anass <manas8269@gmail.com>
2024-08-02 20:34:34 +0300

    Practise on Nullable & Null Operators

---
+++ C#/Cat-Tasks.CSharpAdvancedPart2/Cat-Tasks.CSharpAdvancedPart2/Book.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cat_Tasks.CSharpAdvancedPart2
+{
+    internal class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string? Publisher { get => publisher; set => publisher = value ?? default; }
+        public int? PublishedYear { get => publishedYear; set => publishedYear = value ?? default; }
+       
+        private string? publisher;
+        private int? publishedYear;
+    }
+}
