f726f7ede6b0b790dc259d1ea8e83f0d930790d5
m-anass <manas8269@gmail.com>
2024-08-06 07:21:11 +0300

    Practice on Pure and Impure functions (LINQ-part1)

---
+++ DB/LINQ/Tasks/Tasks.Part-1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Tasks.Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            var result = ints.PureFunc();
+            foreach (var item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ DB/LINQ/Tasks/Tasks.Part-1/UnderstandingPure&ImpureFunctions-01.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tasks.Part_1
+{
+    internal static class UnderstandingPure_ImpureFunctions_01
+    {
+        
+        private static bool isPure = true; // global variable 
+
+        // Write a pure function that filters a list of integers using LINQ.
+        public static List<int> PureFunc(this List<int> Numbers)
+        {
+            List<int> EvenNum = Numbers.Where(x => (x % 2 == 0)).ToList();
+            return EvenNum;
+        }
+
+        // Implement an impure function that modifies a global variable.
+        public static void ImpureFunc()
+        {
+            isPure = false; // imutate global variable
+        }
+    }
+}
