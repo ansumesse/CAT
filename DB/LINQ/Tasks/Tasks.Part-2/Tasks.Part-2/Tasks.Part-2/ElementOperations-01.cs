3977fd6925fb63fa45827cebadbbc05597545c2e
m-anass <manas8269@gmail.com>
2024-08-09 18:43:26 +0300

    Practice on Element Operations (LINQ-part2)

---
+++ DB/LINQ/Tasks/Tasks.Part-2/Tasks.Part-2/Tasks.Part-2/ElementOperations-01.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tasks.Part_2
+{
+    public static class ElementOperations_01
+    {
+        // Implement a LINQ query to retrieve the first and last element from a collection.
+        public static Tsource GetFirstElement<Tsource>(this IEnumerable<Tsource> source)
+        {
+            return (from item in source
+                    select item).First();
+        }
+        public static Tsource GetLastElement<Tsource>(this IEnumerable<Tsource> source)
+        {
+            return (from item in source
+                    select item).Last();
+        }
+    }
+}
---
+++ DB/LINQ/Tasks/Tasks.Part-2/Tasks.Part-2/Tasks.Part-2/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Tasks.Part_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Employee = Repository.LoadEmployees();
+            Console.WriteLine(Employee.GetFirstElement());
+            Console.WriteLine(Employee.GetLastElement());
+        }
+    }
+}
