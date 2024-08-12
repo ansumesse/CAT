91dcf775a3ea2eec1914b53aa18de718fa29411c
m-anass <manas8269@gmail.com>
2024-08-13 02:29:01 +0300

    Practise on Adding My Own Migration

--- DB/EF core/EF-Core-Tasks/EF-Core-Tasks.Part-1/obj/Debug/net8.0/EF-Core-Tasks.Part-1.AssemblyInfo.cs
+++ DB/EF core/EF-Core-Tasks/EF-Core-Tasks.Part-1/obj/Debug/net8.0/EF-Core-Tasks.Part-1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EF-Core-Tasks.Part-1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fb129b2d9d0493cbf58a63ea773546180c743f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d4f657de0689c658551c525f9cc457a9b4568f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EF-Core-Tasks.Part-1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EF-Core-Tasks.Part-1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
