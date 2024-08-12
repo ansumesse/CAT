82bc52e247cea2127390f7e9ed6df4b0c316c3d4
m-anass <manas8269@gmail.com>
2024-08-13 01:50:31 +0300

    Practise on Adding DbContext and Connection String

---
+++ DB/EF core/EF-Core-Tasks/EF-Core-Tasks.Part-1/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_Core_Tasks.Part_1
+{
+    // Create a new DbContext class and configure database connection strings in the application configuration file.
+    internal class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=EFCoreLearning;Integrated Security=True");
+        }
+    }
+}
---
+++ DB/EF core/EF-Core-Tasks/EF-Core-Tasks.Part-1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EF_Core_Tasks.Part_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
